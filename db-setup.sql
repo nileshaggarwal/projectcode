fea3ed5dc36140c2d2591b9b0d49505a07d35f7d
Nilesh Aggarwal <aggarwalnilesh6@gmail.com>
2021-12-20 16:11:33 +0530

    Update db-setup.sql

--- db-setup.sql
+++ db-setup.sql
@@ -2,50 +2,6 @@ CREATE DATABASE LoginSystem;
 
 use LoginSystem;
 
-CREATE TABLE IF NOT EXISTS `users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `name` varchar(50) NOT NULL,
- `email` varchar(50) NOT NULL,
- `password` varchar(50) NOT NULL,
- `create_datetime` datetime NOT NULL,
- `otp` int(6) NOT NULL,
- `status` varchar(10) NOT NULL,
- `role` varchar(8) NOT NULL,
- `phone` int(15),
- PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `accessories`(
-    `name` varchar(50) NOT NULL,
-    `product_id` int(11),
-    `material_type` varchar(20) NOT NULL,
-    `price` int(5) NOT NULL,
-    `car_type` varchar(10) NOT NULL,
-    PRIMARY KEY(`product_id`)
-);
-
-CREATE TABLE IF NOT EXISTS `purchases`(
-    `purchase_id` int(11) NOT NULL,
-    `vehicle_id` int,
-    `user_id` int,
-    `purchaser_name` varchar(20) NOT NULL,
-    `purchase_date` DATE NOT NULL,
-    `address` varchar(200) NOT NULL,
-    `payment_mode` varchar(10) NOT NULL,
-    PRIMARY KEY(`purchase_id`),
-    FOREIGN KEY (`vehicle_id`) REFERENCES `car`(`vehicle_id`),
-    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `purchaseinfor`(
-    `cardnumber` int(16) NOT NULL,
-    `purchase_id` int,
-    `nameoncard` varchar(10) NOT NULL,
-    `expirymm` int(2) NOT NULL,
-    `expiryy` int(4) NOT NULL,
-    FOREIGN KEY (`purchase_id`) REFERENCES `purchases`(`purchase_id`)
-);
-
 CREATE TABLE IF NOT EXISTS `DEALER`(
     `dealerID` int(4)  NOT NULL AUTO_INCREMENT,
     `DName` varchar(50) NOT NULL,
