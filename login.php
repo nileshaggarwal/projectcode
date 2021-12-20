f2b5c5b9818ea9fc4e21854dd7667bd6d45de581
Nilesh Aggarwal <aggarwalnilesh6@gmail.com>
2021-12-20 17:02:29 +0530

    push

--- login.php
+++ login.php
@@ -106,7 +106,7 @@ if(isset($_POST["submit"]))
     <!--BOOTSTRAP CDN-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="./login.css" />
+    <link rel="stylesheet" href="./css/login.css?v=<?php echo time(); ?>" />
 
     <head>
 
